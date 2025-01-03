53ad857f353d99f4043cf9d99200b80cf6a777f7
rikhil916 <chetanrikhil916@gmail.com>
2025-01-03 15:28:26 +0530

    fir

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Add this line, replace 'e-commerce' with your repo name
+  base: '/e-commerce',  // Add this line, replace 'e-commerce' with your repo name
 });

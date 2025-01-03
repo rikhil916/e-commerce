5f8b3c3055560035f919c3415bedc12f7492bcd2
rikhil916 <chetanrikhil916@gmail.com>
2025-01-03 15:13:40 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/e-commerce/',  // Add this line, replace 'e-commerce' with your repo name
 });

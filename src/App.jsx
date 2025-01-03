7b385f047a9f52ef81cfa02358abb00a6931c5a9
rikhil916 <chetanrikhil916@gmail.com>
2025-01-03 15:20:10 +0530

    th

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Signup />}/>
+      <Route path="/e-commerce" element={<Signup />}/>
     </Routes>
     </BrowserRouter>
   );
